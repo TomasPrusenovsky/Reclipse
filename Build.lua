bf08583f5d514261af45805cbbb6427f059247a4
Tomáš Prusenovský <tomasp0303@gmial.com>
2024-06-26 11:49:16 +0200

    Fixed indentation in premake files

--- Build.lua
+++ Build.lua
@@ -1,17 +1,24 @@
+include "Dependencies.lua"
+
 -- premake5.lua
 workspace "Reclipse"
-   architecture "x64"
-   configurations { "Debug", "Release", "Dist" }
-   startproject "Reclipse-App"
+    architecture "x64"
+    configurations { "Debug", "Release", "Dist" }
+    startproject "Reclipse-App"
 
-   -- Workspace-wide build options for MSVC
-   filter "system:windows"
-      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+    -- Workspace-wide build options for MSVC
+    filter "system:windows"
+        buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
 
 OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
 
+group "Dependencies"
+    include "Reclipse/Vendor/GLFW"
+    include "Reclipse/Vendor/glad"
+group ""
+
 group "Reclipse"
-   include "Reclipse/Build-Reclipse.lua"
+    include "Reclipse/Build-Reclipse.lua"
 group ""
 
 include "Reclipse-App/Build-Reclipse-App.lua"
--- Reclipse/Build-Reclipse.lua
+++ Reclipse/Build-Reclipse.lua
@@ -1,37 +1,46 @@
 project "Reclipse"
-   kind "StaticLib"
-   language "C++"
-   cppdialect "C++20"
-   targetdir "Binaries/%{cfg.buildcfg}"
-   staticruntime "off"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "Binaries/%{cfg.buildcfg}"
+    staticruntime "off"
 
-   files { "Source/**.h", "Source/**.cpp" }
+    files { "Source/**.h", "Source/**.cpp" }
 
-   includedirs
-   {
-      "Source"
-   }
+    includedirs
+    {
+        "Source",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.glad}"
+    }
 
-   targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
-   objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+    links
+    {
+        "GLFW",
+        "glad",
+        "opengl32.lib"
+    }
 
-   filter "system:windows"
-       systemversion "latest"
-       defines { }
+    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
-   filter "configurations:Debug"
-       defines { "DEBUG" }
-       runtime "Debug"
-       symbols "On"
+    filter "system:windows"
+        systemversion "latest"
+        defines { }
 
-   filter "configurations:Release"
-       defines { "RELEASE" }
-       runtime "Release"
-       optimize "On"
-       symbols "On"
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "On"
 
-   filter "configurations:Dist"
-       defines { "DIST" }
-       runtime "Release"
-       optimize "On"
-       symbols "Off"
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        runtime "Release"
+        optimize "On"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
